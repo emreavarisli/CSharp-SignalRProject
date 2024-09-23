c16a947922454392bea34b9fab25442d7c66e652
Hasan Emre AVARİSLİ <emreavarisli@gmail.com>
2024-09-23 22:40:41 +0300

    mappinglerin tanımlanması

---
+++ SignalRApi/Mapping/BookingMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.BookingDto;
+using SignalR.DtoLayer.CategoryDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class BookingMapping : Profile
+    {
+        public BookingMapping()
+        {
+            CreateMap<Booking, ResultBookingDto>().ReverseMap();
+            CreateMap<Booking, GetBookingDto>().ReverseMap();
+            CreateMap<Booking, CreateBookingDto>().ReverseMap();
+            CreateMap<Booking, UpdateBookingDto>().ReverseMap();
+        }
+    }
+}
